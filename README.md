c097850f63b20b3a69ea84a8c8b8abc4ffbda288
Udi h Bauman <dibaunaumh@gmail.com>
2021-06-04 13:57:05 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,19 @@
+## Example of bldg batteries
+
+
+### Simple clock battery example
+
+* This is a simple implementation of a bldg battery, with a simple clock protocol
+
+* To install: 
+```
+cd sample-battery
+pip install fastapi
+pip install uvicorn[standard]
+```
+
+* To run: 
+```
+cd sample-battery
+uvicorn main:app --reload
+```
--- sample-battery/main.py
+++ sample-battery/main.py
@@ -8,10 +8,11 @@ app = FastAPI()
 
 
 class Settings(BaseSettings):
-    bldg_server_url: str = "Awesome API"
-    battery_type: str = "Awesome API"
-    battery_vendor: str = "Awesome API"
-    battery_version: str = "Awesome API"
+    bldg_server_url: str = "https://api.w2m.site"
+    bldg_address: str = "g-b(17,24)-l0-b(12,2)"
+    battery_type: str = "sample-clock"
+    battery_vendor: str = "w2m"
+    battery_version: str = "0.0.1"
     
 
 class Message(BaseModel):
