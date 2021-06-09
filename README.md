338ff845beffd1a932b0e6b65bbc5d2b52da8ca6
Udi h Bauman <dibaunaumh@gmail.com>
2021-06-08 18:25:31 -0700

    battery now attaching to bldg

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Example of bldg batteries
+## Examples of bldg batteries
 
 
 ### Simple clock battery example
@@ -13,13 +13,14 @@ pip install uvicorn[standard]
 pip install python-dotenv
 ```
 
-* To configure: create a `.env` file, such as:
+* To configure: create a `.env` file in the sample-battery folder, e.g.:
 ```
 BLDG_SERVER_URL="https://api.w2m.site"
 BLDG_ADDRESS="g-b(17,24)-l0-b(12,2)"
 BATTERY_TYPE="clock-battery"
 BATTERY_VENDOR="w2m"
 BATTERY_VERSION="0.0.1"
+CALLBACK_URL="http://localhost:3000/v1/on_message"
 ```
 
 * To run: 
--- sample-battery/main.py
+++ sample-battery/main.py
@@ -1,8 +1,11 @@
+from bldg_utils import get_flr
 from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pydantic import BaseSettings
+import requests
+import json
 
 
 class Settings(BaseSettings):
@@ -11,6 +14,7 @@ class Settings(BaseSettings):
     battery_type: str = "sample-clock"
     battery_vendor: str = "w2m"
     battery_version: str = "0.0.1"
+    callback_url: str = "http://localhost:3000/v1/on_message"
 
     class Config:
         env_file = ".env"
@@ -27,6 +31,27 @@ class Message(BaseModel):
 app = FastAPI()
 settings = Settings()
 
+
+@app.on_event("startup")
+async def startup_event():
+    # attach to bldg-server
+    battery_config = {
+        "battery_type": settings.battery_type,
+        "battery_vendor": settings.battery_vendor,
+        "battery_version": settings.battery_version,
+        "bldg_address": settings.bldg_address,
+        "callback_url": settings.callback_url,
+        "flr": get_flr(settings.bldg_address)
+    }
+    data = {"battery": battery_config}
+    url = f'{settings.bldg_server_url}/v1/batteries/attach'
+    print(f'Connecting to bldg-server {url}...')
+    r = requests.post(url, json=data)
+    if r.status_code == 422:
+        raise RuntimeError(f'Failed to start battery - another battery is already attached at {settings.bldg_address}')
+    elif r.status_code != 201:
+        raise RuntimeError(f'Failed to start battery - got {r.status_code} error from bldg server: {r.text}')
+    print(f'Battery attached to bldg {settings.bldg_address}')
 
 @app.post("/v1/on_message")
 def process_message(msg: Message):
